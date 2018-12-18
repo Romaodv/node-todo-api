e52c1018975920e42192aa83a98ac1c5b210417d
Guilherme Romão <guilherme.oromao@gmail.com>
2018-12-18 10:41:03 -0200

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -34,8 +34,6 @@ app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
     if (!ObjectID.isValid(id))
         return res.status(404).send();
-    // validate id using isValid
-        // 404 - send back empty
 
 
     Todo.findById(id).then((todo) => {
@@ -43,12 +41,6 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send();
         return res.status(200).send({todo});
     }).catch((e) => res.status(400).send());
-    // findById
-        // success
-            // if todo - send it back
-            // if no todo - send back 404 with empty body
-        // error
-            //400 - and send empty body back
 });
 
 app.listen(3000, () => {
